8e8ed3d5fa2986928de5427bfa18405bee6333fe
uPiscium <130824002+uPiscium@users.noreply.github.com>
2023-12-07 21:13:07 +0900

    Reset branch.

--- GeoFrame/defines.hpp
+++
@@ -1,9 +0,0 @@
-#ifdef GEOFRAME_EXPORT
-#define GEOFRAME_API __declspec(dllexport)
-#else
-#define GEOFRAME_API __declspec(dllimport)
-#endif
-
-#ifdef GEOFRAME_INLINE
-#define GEOFRAME_API GEOFRAME_API inline
-#endif
