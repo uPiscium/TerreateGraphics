41f1863c2350f9d47c97ef72be797b1d7edb0b7a
uPiscium <130824002+uPiscium@users.noreply.github.com>
2023-12-10 14:55:23 +0900

    Add kernel object base.

---
+++ includes/kernel/rawObject.hpp
@@ -0,0 +1,11 @@
+#include "../defines.hpp"
+
+namespace GeoFrame {
+namespace Kernel {
+class RawObject {
+  public:
+    RawObject();
+    ~RawObject();
+};
+} // namespace Kernel
+} // namespace GeoFrame
