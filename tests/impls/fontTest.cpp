09c7d89bba2d525737c1ab628fe3e8a3ce3b770e
uPiscium <130824002+uPiscium@users.noreply.github.com>
2023-12-19 10:13:30 +0900

    Add raw font test.

---
+++ tests/impls/fontTest.cpp
@@ -0,0 +1,52 @@
+#include "../includes/fontTest.hpp"
+
+void font_drawing_test() {
+    GeoFrameContext context;
+
+    Kernel::RawWindow window(800, 600, "Buffer Drawing Test",
+                             Kernel::WindowSettings());
+    window.Bind();
+    Kernel::RawBuffer buffer(BufferUsage::STATIC_DRAW);
+
+    auto attrs = Attribute::GenerateAttributes({2, 2});
+    buffer.LoadVertices({-0.5f, -0.5f, 0.0f, 0.0f, 0.5f, -0.5f, 1.0f, 0.0f,
+                         0.5f, 0.5f, 1.0f, 1.0f, -0.5f, 0.5f, 0.0f, 1.0f});
+    buffer.LoadAttributes(attrs);
+    buffer.LoadIndices({0, 1, 2, 2, 3, 0});
+
+    Kernel::RawShader shader;
+    shader.AddVertexShaderSource("#version 330 core\n"
+                                 "layout (location = 0) in vec2 pos;\n"
+                                 "layout (location = 1) in vec2 tex;\n"
+                                 "out vec2 texCoord;\n"
+                                 "void main() {\n"
+                                 "    gl_Position = vec4(pos, 0.0, 1.0);\n"
+                                 "    texCoord = tex;\n"
+                                 "}\n");
+    shader.AddFragmentShaderSource("#version 330 core\n"
+                                   "in vec2 texCoord;\n"
+                                   "out vec4 color;\n"
+                                   "uniform sampler2D tex;\n"
+                                   "void main() {\n"
+                                   "    color = texture(tex, texCoord);\n"
+                                   "}\n");
+    shader.Compile();
+    shader.Use();
+    context.ActiveTexture(TextureTargets::TEX_1);
+    shader.SetInt("tex", 0);
+
+    Kernel::RawFont font("C:/Windows/Fonts/arial.ttf", 48);
+    font.LoadCharacter(L'A');
+    auto charData = font.GetCharacter(L'A');
+
+    while (!window.IsClosed()) {
+        context.PollEvents();
+        window.Fill({0, 0, 1});
+        window.Clear((int)BufferBit::COLOR_BUFFER);
+        shader.Use();
+        charData->texture->Bind();
+        buffer.Draw(DrawMode::TRIANGLES);
+        charData->texture->Unbind();
+        window.Swap();
+    }
+}
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,4 +1,5 @@
 #include "includes/drawingTest.hpp"
+#include "includes/fontTest.hpp"
 #include "includes/textureTest.hpp"
 #include "includes/windowTest.hpp"
 
@@ -6,5 +7,6 @@ int main() {
     // window_generation_test(800, 600, "Window Test");
     // buffer_drawing_test();
     // texture_drawing_test();
+    font_drawing_test();
     return 0;
 }
