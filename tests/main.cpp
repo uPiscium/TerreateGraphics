64ab0e865dbcbacd1d89b7485c73f1b768737eb6
uPiscium <130824002+uPiscium@users.noreply.github.com>
2023-12-19 12:41:41 +0900

    Add raw screen test.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,6 @@
 #include "includes/drawingTest.hpp"
 #include "includes/fontTest.hpp"
+#include "includes/screenTest.hpp"
 #include "includes/textureTest.hpp"
 #include "includes/windowTest.hpp"
 
@@ -7,6 +8,7 @@ int main() {
     // window_generation_test(800, 600, "Window Test");
     // buffer_drawing_test();
     // texture_drawing_test();
-    font_drawing_test();
+    // font_drawing_test();
+    screen_drawing_test();
     return 0;
 }
