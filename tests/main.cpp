aab62f48f09e5bc4ebefdb5314e1414e584aedac
uPiscium <130824002+uPiscium@users.noreply.github.com>
2023-12-20 10:51:21 +0900

    Change testing.

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,8 +7,8 @@
 int main() {
     // window_generation_test(800, 600, "Window Test");
     // buffer_drawing_test();
-    // texture_drawing_test();
+    texture_drawing_test();
     // font_drawing_test();
-    screen_drawing_test();
+    // screen_drawing_test();
     return 0;
 }
