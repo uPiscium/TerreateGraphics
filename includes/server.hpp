130e9f538382a3ac0cc734dd0a9bdf234c7ee8c5
uPiscium <upiscium@gmail.com>
2024-01-19 12:46:10 +0900

    Add server and client and delete taskflow.

---
+++ includes/server.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "defines.hpp"
+#include "object.hpp"
+
+namespace GeoFrame {
+class Server : public Geobject {
+private:
+};
+} // namespace GeoFrame
