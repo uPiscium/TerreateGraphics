ae4376071f095f035db89ae57744a8aad294ea0e
uPiscium <upiscium@gmail.com>
2024-02-05 20:36:32 +0900

    Add Body class.

--- includes/physics.hpp
+++ includes/physics.hpp
@@ -1,7 +1,25 @@
 #pragma once
 #include "defines.hpp"
 #include "exceptions.hpp"
+#include "object.hpp"
 
 namespace GeoFrame {
-namespace Physics {} // namespace Physics
+namespace Physics {
+using unit = double;
+using Mass = unit;
+using Volume = unit;
+using Restitution = unit;
+using Friction = unit;
+
+using Position = vec3T<unit>;
+using Force = vec3T<unit>;
+using Acceleration = vec3T<unit>;
+using Velocity = vec3T<unit>;
+
+class Body : public Geobject {
+private:
+  Position mPosition;
+};
+
+} // namespace Physics
 } // namespace GeoFrame
