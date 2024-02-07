e460077880a1e358a66e430dacf82dbe720cc0f7
uPiscium <upiscium@gmail.com>
2024-02-07 16:52:12 +0900

    Move AABB and BS to physics file.

--- includes/physics.hpp
+++ includes/physics.hpp
@@ -5,20 +5,67 @@
 
 namespace GeoFrame {
 namespace Physics {
-using unit = double;
-using Mass = unit;
-using Volume = unit;
-using Restitution = unit;
-using Friction = unit;
+class BoundingSphere {
+private:
+  Position mCenter;
+  float mRadius;
 
-using Position = vec3T<unit>;
-using Force = vec3T<unit>;
-using Acceleration = vec3T<unit>;
-using Velocity = vec3T<unit>;
+public:
+  BoundingSphere(Position const &center, float const &radius)
+      : mCenter(center), mRadius(radius) {}
+  BoundingSphere(BoundingSphere const &other)
+      : mCenter(other.mCenter), mRadius(other.mRadius) {}
+
+  Position GetCenter() const { return mCenter; }
+  float GetRadius() const { return mRadius; }
+
+  void SetCenter(Position const &center) { mCenter = center; }
+  void SetRadius(float const &radius) { mRadius = radius; }
+
+  bool IsColliding(BoundingSphere const &other) const;
+
+  BoundingSphere &operator=(BoundingSphere const &other);
+};
+
+class AxisAlignedBoundingBox {
+private:
+  Position mMax;
+  Position mMin;
+
+public:
+  AxisAlignedBoundingBox(Position const &max, Position const &min)
+      : mMax(max), mMin(min) {}
+  AxisAlignedBoundingBox(AxisAlignedBoundingBox const &other)
+      : mMax(other.mMax), mMin(other.mMin) {}
+
+  Position GetMax() const { return mMax; }
+  Position GetMin() const { return mMin; }
+  Position GetCenter() const;
+  float GetXRange() const { return mMax[0] - mMin[1]; }
+  float GetYRange() const { return mMax[1] - mMin[1]; }
+  float GetZRange() const { return mMax[2] - mMin[2]; }
+
+  BoundingSphere AcquireBoundingSphere() const;
+
+  void SetMax(Position const &max) { mMax = max; }
+  void SetMin(Position const &min) { mMin = min; }
+
+  bool IsColliding(AxisAlignedBoundingBox const &other) const;
+
+  AxisAlignedBoundingBox &operator=(AxisAlignedBoundingBox const &other);
+};
+
+using AABB = AxisAlignedBoundingBox;
+using BS = BoundingSphere;
 
 class Body : public Geobject {
 private:
   Position mPosition;
+  AABB mAABB;
+  BS mBS;
+
+public:
+  Body() : mPosition(0, 0, 0), mAABB({0, 0, 0}, {0, 0, 0}), mBS({0, 0, 0}, 0) {}
 };
 
 } // namespace Physics
