2158c761e338173a2bd73486ab7e739532b0cf51
uPiscium <upiscium@gmail.com>
2024-02-14 19:42:41 +0900

    Fix hoge hoge.

--- includes/physics.hpp
+++ includes/physics.hpp
@@ -2,18 +2,8 @@
 #include "defines.hpp"
 #include "exceptions.hpp"
 #include "object.hpp"
+#include "primitive.hpp"
 
 namespace GeoFrame {
-namespace Physics {
-class Body : public Geobject {
-private:
-  Position mPosition;
-  AABB mAABB;
-  BS mBS;
-
-public:
-  Body() : mPosition(0, 0, 0), mAABB({0, 0, 0}, {0, 0, 0}), mBS({0, 0, 0}, 0) {}
-};
-
-} // namespace Physics
+namespace Physics {} // namespace Physics
 } // namespace GeoFrame
