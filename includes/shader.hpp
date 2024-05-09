2991b9f1545fb2e328ad234cd9f687aedd0d64fb
uPiscium <upiscium@gmail.com>
2024-05-09 16:02:25 +0900

    Fix shader uniform functions and fix memory alignment.

--- includes/shader.hpp
+++ includes/shader.hpp
@@ -140,7 +140,7 @@ public:
    */
   void SetMat2(Str const &name, mat2 const &value) const {
     glUniformMatrix2fv(glGetUniformLocation(mShaderID, name.c_str()), 1,
-                       GL_FALSE, (float const *)value);
+                       GL_FALSE, (float const *)(value.AcquireTransposed()));
   }
   /*
    * @brief: Setter for shader mat2x3 uniform.
@@ -149,7 +149,7 @@ public:
    */
   void SetMat2x3(Str const &name, mat2x3 const &value) const {
     glUniformMatrix2x3fv(glGetUniformLocation(mShaderID, name.c_str()), 1,
-                         GL_FALSE, (float const *)value);
+                         GL_FALSE, (float const *)(value.AcquireTransposed()));
   }
   /*
    * @brief: Setter for shader mat2x4 uniform.
@@ -158,7 +158,7 @@ public:
    */
   void SetMat2x4(Str const &name, mat2x4 const &value) const {
     glUniformMatrix2x4fv(glGetUniformLocation(mShaderID, name.c_str()), 1,
-                         GL_FALSE, (float const *)value);
+                         GL_FALSE, (float const *)(value.AcquireTransposed()));
   }
   /*
    * @brief: Setter for shader mat3x2 uniform.
@@ -167,7 +167,7 @@ public:
    */
   void SetMat3x2(Str const &name, mat3x2 const &value) const {
     glUniformMatrix3x2fv(glGetUniformLocation(mShaderID, name.c_str()), 1,
-                         GL_FALSE, (float const *)value);
+                         GL_FALSE, (float const *)(value.AcquireTransposed()));
   }
   /*
    * @brief: Setter for shader mat3 uniform.
@@ -176,7 +176,7 @@ public:
    */
   void SetMat3(Str const &name, mat3 const &value) const {
     glUniformMatrix3fv(glGetUniformLocation(mShaderID, name.c_str()), 1,
-                       GL_FALSE, (float const *)value);
+                       GL_FALSE, (float const *)(value.AcquireTransposed()));
   }
   /*
    * @brief: Setter for shader mat3x4 uniform.
@@ -185,7 +185,7 @@ public:
    */
   void SetMat3x4(Str const &name, mat3x4 const &value) const {
     glUniformMatrix3x4fv(glGetUniformLocation(mShaderID, name.c_str()), 1,
-                         GL_FALSE, (float const *)value);
+                         GL_FALSE, (float const *)(value.AcquireTransposed()));
   }
   /*
    * @brief: Setter for shader mat4x2 uniform.
@@ -194,7 +194,7 @@ public:
    */
   void SetMat4x2(Str const &name, mat4x2 const &value) const {
     glUniformMatrix4x2fv(glGetUniformLocation(mShaderID, name.c_str()), 1,
-                         GL_FALSE, (float const *)value);
+                         GL_FALSE, (float const *)(value.AcquireTransposed()));
   }
   /*
    * @brief: Setter for shader mat4x3 uniform.
@@ -203,7 +203,7 @@ public:
    */
   void SetMat4x3(Str const &name, mat4x3 const &value) const {
     glUniformMatrix4x3fv(glGetUniformLocation(mShaderID, name.c_str()), 1,
-                         GL_FALSE, (float const *)value);
+                         GL_FALSE, (float const *)(value.AcquireTransposed()));
   }
   /*
    * @brief: Setter for shader mat4 uniform.
@@ -212,7 +212,7 @@ public:
    */
   void SetMat4(Str const &name, mat4 const &value) const {
     glUniformMatrix4fv(glGetUniformLocation(mShaderID, name.c_str()), 1,
-                       GL_FALSE, (float const *)value);
+                       GL_FALSE, (float const *)(value.AcquireTransposed()));
   }
   /*
    * @brief: Setter for blending function.
