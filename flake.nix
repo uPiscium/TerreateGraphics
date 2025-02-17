746336a7feaa6c7807862d174fde336c11407cec
uPiscium <upiscium@gmail.com>
2025-02-17 15:43:23 +0900

    Support flake.

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.pkg-config
+          pkgs.wayland
+          pkgs.xorg.libX11
+          pkgs.xorg.libXcursor
+          pkgs.xorg.libXrandr
+          pkgs.xorg.libXi
+          pkgs.libxkbcommon
+        ];
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+          wayland
+          libglvnd
+          libxkbcommon
+        ]);
+      };
+    };
+}
+
